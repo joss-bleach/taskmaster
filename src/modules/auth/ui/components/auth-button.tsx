6a963057f2a536fb5b1e9a324ef1c6917d88f8b6
Joss Bleach <joss.bleach@outlook.com>
2025-02-21 17:25:02 +0000

    fixed auth button

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -6,10 +6,11 @@ import { Button } from "@/components/ui/button";
 
 export const AuthButton = () => {
   const pathname = usePathname();
+  console.log(pathname);
   return (
-    <Button className="hover:bg-primary/90 bg-primary text-secondary flex h-[40px] w-full items-center justify-center space-x-2 px-6 py-4 text-sm font-medium transition-colors focus-visible:outline-none active:scale-[0.98] disabled:pointer-events-none disabled:opacity-50">
-      <Link href={pathname === "sign-in" ? "/sign-up" : "/sign-in"}>
-        {pathname === "sign-in" ? "Sign up" : "Sign in"}
+    <Button className="hover:bg-primary/90 bg-primary text-secondary flex h-[40px] w-[100px] items-center justify-center space-x-2 px-6 py-4 text-sm font-medium transition-colors focus-visible:outline-none active:scale-[0.98] disabled:pointer-events-none disabled:opacity-50">
+      <Link href={pathname === "/sign-in" ? "/sign-up" : "/sign-in"}>
+        {pathname === "/sign-in" ? "Sign up" : "Sign in"}
       </Link>
     </Button>
   );
