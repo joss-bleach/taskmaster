e8e693b4168b8455ef1e057114b252519109f053
Joss Bleach <joss.bleach@outlook.com>
2025-03-04 13:48:36 +0000

    auth setup

---
+++ src/config/env.ts
@@ -0,0 +1,18 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    BETTER_AUTH_SECRET: z.string().min(1),
+    BETTER_AUTH_URL: z.string().min(1),
+    DATABASE_URL: z.string().min(1),
+    NODE_ENV: z.string().min(1).default("development"),
+  },
+  client: {},
+  runtimeEnv: {
+    BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
+    NODE_ENV: process.env.NODE_ENV,
+  },
+});
---
+++ src/db/index.ts
@@ -0,0 +1,14 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+
+import { env } from "@/config/env";
+
+import * as schema from "./schema";
+
+const client = postgres(env.DATABASE_URL);
+
+export const db = drizzle(client, {
+  schema,
+  logger: env.NODE_ENV === "development" ? true : false,
+});
+export type db = typeof db;
